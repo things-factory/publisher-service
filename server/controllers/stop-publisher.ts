de01c42158dc00ccce8d3fc4a3a2e0818774ec54
Stellan Gil <lotstar87@gmail.com>
2019-08-21 18:11:15 +0900

    add services - start-publisher, stop-publisher

---
+++ server/controllers/stop-publisher.ts
@@ -0,0 +1,24 @@
+import { updatePublisher } from '../graphql/resolvers/publisher/update-publisher'
+
+export async function stopPublisher(id, context) {
+  var globalObject = global as any
+  if (!globalObject.jobs) globalObject.jobs = {}
+
+  var job = globalObject.jobs[id]
+  if (job) job.stop()
+
+  updatePublisher.updatePublisher(
+    null,
+    {
+      id,
+      patch: {
+        status: 0
+      }
+    },
+    context
+  )
+
+  return {
+    id
+  }
+}
--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,18 @@
 import { publishers } from './controllers/publishers'
+import { startPublisher } from './controllers/start-publisher'
+import { stopPublisher } from './controllers/stop-publisher'
+import path from 'path'
+import { path as appRootPath } from 'app-root-path'
+
+// var mqttConfig = require(path.resolve(appRootPath, 'mqttconfig'))
+// try {
+//   mqttConfig = require(path.resolve(appRootPath, 'mqttconfig'))
+// } catch (e) {
+//   mqttConfig = require('@things-factory/publisher-service/mqttconfig')
+// }
+
 process.on('bootstrap-module-history-fallback' as any, (app, fallbackOption) => {
-  fallbackOption.whiteList.push('/publishers')
+  fallbackOption.whiteList.push('/publishers', '/start-publisher', '/stop-publisher')
 })
 
 process.on('bootstrap-module-route' as any, (app, routes) => {
@@ -8,6 +20,21 @@ process.on('bootstrap-module-route' as any, (app, routes) => {
     context.body = {
       success: true,
       ...(await publishers())
+    }
+  })
+
+  routes.get('/start-publisher/:id', async (context, next) => {
+    var id = context.params.id
+    context.body = {
+      success: true,
+      ...(await startPublisher(id, context))
+    }
+  })
+  routes.get('/stop-publisher/:id', async (context, next) => {
+    var id = context.params.id
+    context.body = {
+      success: true,
+      ...(await stopPublisher(id, context))
     }
   })
 })
