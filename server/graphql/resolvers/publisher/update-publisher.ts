b4f594ca19e673fd56f9e3148fb9699e293a38bf
Stellan Gil <lotstar87@gmail.com>
2019-08-22 15:44:27 +0900

    remove unnecessary logic

--- server/graphql/resolvers/publisher/update-publisher.ts
+++ server/graphql/resolvers/publisher/update-publisher.ts
@@ -4,15 +4,7 @@ import { Publisher } from '../../../entities'
 export const updatePublisher = {
   async updatePublisher(_, { id, patch }, context) {
     const repository = getRepository(Publisher)
-
     const publisher = await repository.findOne({ id })
-
-    if (patch.model) {
-      const { width, height } = JSON.parse(patch.model)
-
-      patch.width = width
-      patch.height = height
-    }
 
     return await repository.save({
       ...publisher,
